d7c1260b81ed67f8f16d63ee6cb68ec456e7378a
clauterra <38335060+clauterra@users.noreply.github.com>
2020-05-23 09:32:37 -0300

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 window.appConf = {
-    author: 'React Drive CMS - cvlp',
+    author: 'React Drive CMS',
     dashboardId: '1jH1jTFwpjDiz0TupklI3eIuF0tRgB5dxLPPSE_sPQOM',
     sendContactMessageUrlId:
         'AKfycbzHgUx_utrTFWtNCwwwmouhL0Byv4n_X6dvC2wJ',
